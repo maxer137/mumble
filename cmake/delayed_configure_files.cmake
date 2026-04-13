f6934d0fdc12d882c8b3f797b96a94248cea277d
maxer137 <maxer137@gmail.com>
2026-04-14 01:17:55 +0200

    FORMAT(build): Apply neocmakelsp formatting to CMake files
    Reformat all CMake-related files using neocmakelsp to ensure
    consistent style across the project.
    
    This change only affects formatting and does not modify build
    logic or behavior.
    
    All CMakeLists.txt and *.cmake files have been processed to
    match the current formatting rules enforced in CI.

--- cmake/delayed_configure_files.cmake
+++ cmake/delayed_configure_files.cmake
@@ -21,95 +21,95 @@
 #                                 use of generator expressions.
 # @ONLY                           Passes the @ONLY parameter to the generated configure_file invocations
 function(delayed_configure_files)
-	set(options "@ONLY")
-	set(oneValueArgs TARGET SCRIPT_NAME PYTHON_INTERPRETER)
-	set(multiValueArgs FILES PASSTHROUGH_VARIABLES VARIABLES)
+  set(options "@ONLY")
+  set(oneValueArgs TARGET SCRIPT_NAME PYTHON_INTERPRETER)
+  set(multiValueArgs FILES PASSTHROUGH_VARIABLES VARIABLES)
 
-	cmake_parse_arguments(DELAYED_CONFIGURE_FILES "${options}" "${oneValueArgs}" "${multiValueArgs}" ${ARGN})
+  cmake_parse_arguments(DELAYED_CONFIGURE_FILES "${options}" "${oneValueArgs}" "${multiValueArgs}" ${ARGN})
 
-	if(DELAYED_CONFIGURE_FILES_UNPARSED_ARGUMENTS)
-		message(FATAL_ERROR "Unrecognized arguments to delayed_configure_file: \"${DELAYED_CONFIGURE_FILES_UNPARSED_ARGUMENTS}\"")
-	endif()
-	# Check required args
-	if(NOT DELAYED_CONFIGURE_FILES_FILES)
-		message(FATAL_ERROR "Called delayed_configure_files without the FILES parameter")
-	endif()
-	if(DELAYED_CONFIGURE_FILES_TARGET)
-		if(NOT TARGET ${DELAYED_CONFIGURE_FILES_TARGET})
-			message(FATAL_ERROR "delayed_configure_files: Provided target \"${DELAYED_CONFIGURE_FILES_TARGET}\" is not actually a (valid) target")
-		endif()
-	endif()
-	if(NOT DELAYED_CONFIGURE_FILES_PYTHON_INTERPRETER)
-		message(FATAL_ERROR "Called delayed_configure_files without the PYTHON_INTERPRETER parameter")
-	endif()
-
-
-	# Generate a free target name
-	set(AUX_TARGET_NUM 1)
-	set(AUX_TARGET_BASENAME "delayed_configure_files_target_")
-	while(TARGET "${AUX_TARGET_BASENAME}${AUX_TARGET_NUM}")
-		math(EXPR AUX_TARGET_NUM "${AUX_TARGET_NUM} + 1")
-	endwhile()
-	set(AUX_TARGET_NAME "${AUX_TARGET_BASENAME}${AUX_TARGET_NUM}")
+  if(DELAYED_CONFIGURE_FILES_UNPARSED_ARGUMENTS)
+    message(FATAL_ERROR "Unrecognized arguments to delayed_configure_file: \"${DELAYED_CONFIGURE_FILES_UNPARSED_ARGUMENTS}\"")
+  endif()
+  # Check required args
+  if(NOT DELAYED_CONFIGURE_FILES_FILES)
+    message(FATAL_ERROR "Called delayed_configure_files without the FILES parameter")
+  endif()
+  if(DELAYED_CONFIGURE_FILES_TARGET)
+    if(NOT TARGET ${DELAYED_CONFIGURE_FILES_TARGET})
+      message(FATAL_ERROR "delayed_configure_files: Provided target \"${DELAYED_CONFIGURE_FILES_TARGET}\" is not actually a (valid) target")
+    endif()
+  endif()
+  if(NOT DELAYED_CONFIGURE_FILES_PYTHON_INTERPRETER)
+    message(FATAL_ERROR "Called delayed_configure_files without the PYTHON_INTERPRETER parameter")
+  endif()
 
 
-	# Handle defaults
-	if(NOT DELAYED_CONFIGURE_FILES_SCRIPT_NAME)
-		set(DELAYED_CONFIGURE_FILES_SCRIPT_NAME "configure_files_${AUX_TARGET_NUM}.cmake")
-	endif()
-
-	if(DELAYED_CONFIGURE_FILES_@ONLY)
-		set(AT_ONLY "--at-only")
-	else()
-		set(AT_ONLY "")
-	endif()
-
-	
-	if(DELAYED_CONFIGURE_FILES_VARIABLES)
-		set(VARIABLES ${DELAYED_CONFIGURE_FILES_VARIABLES})
-	else()
-		set(VARIABLES "")
-	endif()
-	if(DELAYED_CONFIGURE_FILES_PASSTHROUGH_VARIABLES)
-		foreach(CURRENT_VAR IN LISTS DELAYED_CONFIGURE_FILES_PASSTHROUGH_VARIABLES)
-			if(NOT DEFINED ${CURRENT_VAR})
-				message(FATAL_ERROR "delayed_configure_files: Can't passthrough non-existing variable \"${CURRENT_VAR}\"")
-			endif()
-
-			list(APPEND VARIABLES "${CURRENT_VAR}=${${CURRENT_VAR}}")
-		endforeach()
-	endif()
-
-	if(NOT VARIABLES STREQUAL "")
-		set(VARIABLES --variables ${VARIABLES})
-	endif()
+  # Generate a free target name
+  set(AUX_TARGET_NUM 1)
+  set(AUX_TARGET_BASENAME "delayed_configure_files_target_")
+  while(TARGET "${AUX_TARGET_BASENAME}${AUX_TARGET_NUM}")
+  math(EXPR AUX_TARGET_NUM "${AUX_TARGET_NUM} + 1")
+ endwhile()
+  set(AUX_TARGET_NAME "${AUX_TARGET_BASENAME}${AUX_TARGET_NUM}")
 
 
+  # Handle defaults
+  if(NOT DELAYED_CONFIGURE_FILES_SCRIPT_NAME)
+    set(DELAYED_CONFIGURE_FILES_SCRIPT_NAME "configure_files_${AUX_TARGET_NUM}.cmake")
+  endif()
 
-	# Create a command to generate an intermediary cmake script that will take care of configuring the respective
-	# files, once executed
-	add_custom_command(
-		OUTPUT "${DELAYED_CONFIGURE_FILES_SCRIPT_NAME}"
-		COMMAND "${DELAYED_CONFIGURE_FILES_PYTHON_INTERPRETER}"
-		ARGS "${CMAKE_SOURCE_DIR}/scripts/generate_configure_cmake_script.py"
-			--files ${DELAYED_CONFIGURE_FILES_FILES} ${VARIABLES}
-			--output "${DELAYED_CONFIGURE_FILES_SCRIPT_NAME}"
-			${AT_ONLY}
-			COMMENT "Generating delayed configure script ${DELAYED_CONFIGURE_FILES_SCRIPT_NAME}"
-	)
+  if(DELAYED_CONFIGURE_FILES_@ONLY)
+    set(AT_ONLY "--at-only")
+  else()
+    set(AT_ONLY "")
+  endif()
 
-	# Create a target that will execute the generated cmake script. Make sure that this target will be
-	# added to the default build target, ensuring that it is always executed.
-	add_custom_target(
-		${AUX_TARGET_NAME} ALL
-		DEPENDS "${DELAYED_CONFIGURE_FILES_SCRIPT_NAME}"
-		COMMAND "${CMAKE_COMMAND}" -P "${DELAYED_CONFIGURE_FILES_SCRIPT_NAME}"
-		COMMENT "Executing delayed configure script ${DELAYED_CONFIGURE_FILES_SCRIPT_NAME}"
-	)
 
-	if(DELAYED_CONFIGURE_FILES_TARGET)
-		# Make our auxiliary target depend on the provided main target to ensure that the main target is always built
-		# before our auxiliary target executes.
-		add_dependencies(${AUX_TARGET_NAME} ${DELAYED_CONFIGURE_FILES_TARGET})
-	endif()
+  if(DELAYED_CONFIGURE_FILES_VARIABLES)
+    set(VARIABLES ${DELAYED_CONFIGURE_FILES_VARIABLES})
+  else()
+    set(VARIABLES "")
+  endif()
+  if(DELAYED_CONFIGURE_FILES_PASSTHROUGH_VARIABLES)
+    foreach(CURRENT_VAR IN LISTS DELAYED_CONFIGURE_FILES_PASSTHROUGH_VARIABLES)
+      if(NOT DEFINED ${CURRENT_VAR})
+        message(FATAL_ERROR "delayed_configure_files: Can't passthrough non-existing variable \"${CURRENT_VAR}\"")
+      endif()
+
+      list(APPEND VARIABLES "${CURRENT_VAR}=${${CURRENT_VAR}}")
+    endforeach()
+  endif()
+
+  if(NOT VARIABLES STREQUAL "")
+    set(VARIABLES --variables ${VARIABLES})
+  endif()
+
+
+
+  # Create a command to generate an intermediary cmake script that will take care of configuring the respective
+  # files, once executed
+  add_custom_command(
+  OUTPUT "${DELAYED_CONFIGURE_FILES_SCRIPT_NAME}"
+  COMMAND "${DELAYED_CONFIGURE_FILES_PYTHON_INTERPRETER}"
+  ARGS "${CMAKE_SOURCE_DIR}/scripts/generate_configure_cmake_script.py"
+   --files ${DELAYED_CONFIGURE_FILES_FILES} ${VARIABLES}
+   --output "${DELAYED_CONFIGURE_FILES_SCRIPT_NAME}"
+   ${AT_ONLY}
+   COMMENT "Generating delayed configure script ${DELAYED_CONFIGURE_FILES_SCRIPT_NAME}"
+ )
+
+  # Create a target that will execute the generated cmake script. Make sure that this target will be
+  # added to the default build target, ensuring that it is always executed.
+  add_custom_target(
+  ${AUX_TARGET_NAME} ALL
+  DEPENDS "${DELAYED_CONFIGURE_FILES_SCRIPT_NAME}"
+  COMMAND "${CMAKE_COMMAND}" -P "${DELAYED_CONFIGURE_FILES_SCRIPT_NAME}"
+  COMMENT "Executing delayed configure script ${DELAYED_CONFIGURE_FILES_SCRIPT_NAME}"
+ )
+
+  if(DELAYED_CONFIGURE_FILES_TARGET)
+    # Make our auxiliary target depend on the provided main target to ensure that the main target is always built
+    # before our auxiliary target executes.
+    add_dependencies(${AUX_TARGET_NAME} ${DELAYED_CONFIGURE_FILES_TARGET})
+  endif()
 endfunction()
--- cmake/install-library.cmake
+++ cmake/install-library.cmake
@@ -4,14 +4,14 @@
 # Mumble source tree or at <https://www.mumble.info/LICENSE>.
 
 function(install_library lib component)
-	get_target_property(lib_type ${lib} TYPE)
+  get_target_property(lib_type ${lib} TYPE)
 
-	if(NOT lib_type STREQUAL "STATIC_LIBRARY")
-		# only install non-static libraries
-		if(WIN32)
-			install(TARGETS ${lib} RUNTIME DESTINATION "${MUMBLE_INSTALL_LIBDIR}" COMPONENT "${component}")
-		else()
-			install(TARGETS ${lib} LIBRARY DESTINATION "${MUMBLE_INSTALL_LIBDIR}" COMPONENT "${component}")
-		endif()
-	endif()
+  if(NOT lib_type STREQUAL "STATIC_LIBRARY")
+    # only install non-static libraries
+    if(WIN32)
+      install(TARGETS ${lib} RUNTIME DESTINATION "${MUMBLE_INSTALL_LIBDIR}" COMPONENT "${component}")
+    else()
+      install(TARGETS ${lib} LIBRARY DESTINATION "${MUMBLE_INSTALL_LIBDIR}" COMPONENT "${component}")
+    endif()
+  endif()
 endfunction()
